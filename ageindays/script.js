fff2644fe19f0401647fce90e06e509571528bc3
kedarnathdev <p.kedarnath27@gmail.com>
2020-12-31 20:33:27 +0530

    gjg

--- ageindays/script.js
+++ ageindays/script.js
@@ -1,2 +1,14 @@
-var ageInDayss;
-
+function ageInDays() {
+  
+  var birthYear = prompt('Enter your Birth Year 😄');
+  ageInDayss = (2020 - birthYear) * 365;
+  
+  var h1 = document.createElement('h1');
+  
+  var textAnswer = document.createTextNode('You are '+ ageInDayss +' Days old !!');
+  
+  h1.setAttribute('id','ageInDays');
+  h1.appendChild(textAnswer);
+  document.getElementById('flex-result').appendChild(h1);
+  
+}
